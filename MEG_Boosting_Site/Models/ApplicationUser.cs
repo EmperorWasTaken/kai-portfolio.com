5f0bf2bedf7eddc66d197fa74cb5f76db223e660
Kai Kåsa <ktkaas18@uia.no>
2022-05-14 20:55:12 +0200

    Fix

--- MEG_Boosting_Site/Models/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace MEG_Boosting_Site.Models
-{
-    public class ApplicationUser : IdentityUser
-    {
-        public string Nickname { get; set; }
-    }
-}
